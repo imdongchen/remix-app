981f3832af071f93dad825de86ece37cd6295f29
Dong Chen <imdongchen@gmail.com>
2024-02-01 15:58:18 +0000

    test push from ipad

--- app/root.tsx
+++ app/root.tsx
@@ -87,7 +87,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction<typeof loader> = ({ data }) => {
 	return [
-		{ title: data ? 'Epic Notes' : 'Error | Epic Notes' },
+		{ title: data ? 'TruckLink' : 'Error | TruckLink' },
 		{ name: 'description', content: `Your own captain's log` },
 	]
 }
@@ -278,10 +278,10 @@ function Logo() {
 	return (
 		<Link to="/" className="group grid leading-snug">
 			<span className="font-light transition group-hover:-translate-x-1	">
-				epic
+			Truck
 			</span>
 			<span className="font-bold transition group-hover:translate-x-1	">
-				notes
+Link
 			</span>
 		</Link>
 	)
